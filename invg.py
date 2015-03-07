4e4e58239b711a1d02f7e5951bf01fdab27012b1
CampbellJohn <j.campb@gmail.com>
2015-03-07 14:42:09 -0800

    First game code. Cleaned up a bit. Took out a few tabs, and replaced the longer game file with run.py, since we'll be typing that shit thousands of times. Game file runs. Try it out.

---
+++ invg.py
@@ -0,0 +1,77 @@
+# This file is a collection of custom classes written for the game.
+# It is imported into ineatvideogame.py
+# In ineatvideogame.py, The Main / Host object instantiates an object of class
+# 'GameSession'. 
+
+# 80 Characters Maximum Width - 80 Characters Maximum Width - 80 Characters ###
+
+class Area:
+    # The __init__ launches automatically when the object is instantiated.
+    # http://www.ibiblio.org/g2swap/byteofpython/read/class-init.html
+    def __init__(self):
+        pass
+
+class DataValidator:
+    def __init__(self):
+        pass
+
+class GameSession:
+
+    # Define the variables for a game session, and assign initial values
+    LoggingLevel = 0
+    GameStatus = 'not started'
+    PausedState = True
+	
+    # The __init__ launches automatically when the object is instantiated.
+    def __init__(self):
+        # Begin setting up the Game Session
+        self.GameStatus = 'initializing'
+	
+        # Get the logging level and pass it as an argument when calling Logger
+        # Instantiate a Logger object to report game logs to.
+        Log = Logger(self.LoggingLevel)
+
+
+class Logger:
+    # The __init__ launches automatically when the object is instantiated.
+    def __init__(self, args):
+        pass
+
+class Menu:
+    # The __init__ launches automatically when the object is instantiated.
+    def __init__(self):
+        # 
+        # Initial Menu:
+        # 
+        # New Game
+        # Load Game
+        # Options
+        # Exit
+        # 
+        # If 'New Game' is selected...
+        # Close the menu dialogue
+        # Display loading screen if necessary
+        # Instantiate the current World, Area, and Room
+		# Instantiate Items, Characters, Quests, etc., other???
+        # 
+        # Hide loading screen, if any
+        # Change the game status to Active
+        # Log the event
+        # Now listening for user input...
+        pass
+
+class Player:
+    # The __init__ launches automatically when the object is instantiated.
+    def __init__(self):
+        pass
+
+class Room:
+    # The __init__ launches automatically when the object is instantiated.
+    def __init__(self):
+        pass
+
+class World:
+    # The __init__ launches automatically when the object is instantiated.
+    def __init__(self):
+        pass
+
---
+++ run.py
@@ -0,0 +1,77 @@
+# Import classes from invg.py (among other things)
+import invg, pygame, sys
+from pygame.locals import *
+
+# Initialize pygame
+pygame.init()
+
+# 80 Characters Maximum Width - 80 Characters Maximum Width - 80 Characters ###
+
+#
+# A Host is an object that has a class of "Main".
+# I suppose the class could also be named Host, 
+# but it hurts my programmer heart not to have a Main().
+# 
+# In any case...
+# Host is, for all intents and purposes, the 'main' object from which all other
+# objects are instantiated.
+#
+# In Alpha we're keeping things simple, and running just one instance
+# of the game, on this machine. 
+# 
+# This machine is the Host, but is also the Client. 
+# The Host automatically starts a game.
+# 
+# In the future, a dedicated Host can allow multiple Clients
+# to connect and each start their own game sessions.
+# The games would be started and stopped by the Clients.
+# The Host could, in emergencies, dump game sessions, or even
+# migrate games to other Hosts (load balancing).
+#
+
+class Main:
+    def __init__(self):
+        pass
+    # The __init__ launches automatically when the object is 
+    # instantiated. Documentation:
+    # http://www.ibiblio.org/g2swap/byteofpython/read/class-init.html
+
+    FPS = 30 # frames per second setting
+    FPSCLOCK = pygame.time.Clock()
+    
+    # Font Info
+    BASICFONT = pygame.font.Font('freesansbold.ttf', 32)
+
+
+    # set up the window
+    DISPLAYSURF = pygame.display.set_mode((960, 704), 0, 32)
+    pygame.display.set_caption('i neat video game')
+
+    helloSurf = BASICFONT.render('Hello Kip. Nice pants.', True, (255,255,255))
+    helloRect = helloSurf.get_rect()
+    helloRect.center = (480, 350)
+
+
+
+    DISPLAYSURF.blit(helloSurf, helloRect) 
+
+
+    # Alpha - Automatically launch a game session
+    Game = invg.GameSession()
+
+    while True: 
+    # The Main Host Loop
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                # Terminate this Host. End the program (and all running Game Sessions!).
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+# Instantiate a Host:
+
+Host = Main()
+
+
